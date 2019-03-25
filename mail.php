5f06e4bd2617ffc619589ee9daeb96aeed4a81d7
Sam1996 <samnishanth01@gmail.com>
2019-03-25 09:09:57 -0400

    calculate.html added

--- mail.php
+++ mail.php
@@ -5,29 +5,21 @@ require 'vendor/autoload.php';
 
 if(isset($_POST)){
     $data = json_decode($_POST['data'],true); 
-    $message=file_get_contents('email_template.php');
-    $electronics = $appliances = $things = "";
-    foreach($data['electronics'] as $key=>$value){
-        $electronics .= "<tr>";
-        $electronics .= "<td class='tg-0pky'>".$key."</td>";
-        $electronics .= "<td class='tg-0pky'>".$value."</td>";
-        $electronics .= "</tr>";
-    }
-    foreach($data['appliances'] as $key=>$value){
-        $appliances .= "<tr>";
-        $appliances .= "<td class='tg-0pky'>".$key."</td>";
-        $appliances .= "<td class='tg-0pky'>".$value."</td>";
-        $appliances .= "</tr>";
-    }
-    foreach($data['things'] as $key=>$value){
-        $things .= "<tr>";
-        $things .= "<td class='tg-0pky'>".$key."</td>";
-        $things .= "<td class='tg-0pky'>".$value."</td>";
-        $things .= "</tr>";
+    $message=file_get_contents('email_template.html');
+
+    $fromToDetails = $data['data_one'];
+    $PropertyDetails = $data['data_two'];
+    $DateDetails = $data['data_three'];
+    $personalDetails = $data['data_four'];    
+
+    if(array_key_exists('city',$fromToDetails)){
+        $area = 'Within '.$fromToDetails['city'];
+    }else{
+        $area = 'Between Cities';
     }
 
-    $replaceFrom = array('[=NAME]','[=EMAIL]','[=PHONE]','[=MODE]','[=SHIFT]','[=FROM]','[=TO]','[=ELECTRONICS]','[=APPLIANCES]','[=OTHERS]');
-    $replaceTo = array($data['info']['name'],$data['info']['email'],$data['info']['phone'],$data['transport']['mode'],$data['transport']['shift'],$data['transport']['from'],$data['transport']['to'],$electronics,$appliances,$things);
+    $replaceFrom = array('[=NAME]','[=EMAIL]','[=PHONE]','[=AREA]','[=TYPE]','[=PROPERTY]','[=FROM]','[=TO]','[=DATE]');
+    $replaceTo = array($personalDetails['name'],$personalDetails['email'],$personalDetails['phone'],$area,$PropertyDetails['type'],$PropertyDetails['roomType'],$fromToDetails['from'],$fromToDetails['to'],$DateDetails['movingDate']);
     $message = str_replace($replaceFrom,$replaceTo,$message);
     
     $mail = new PHPMailer(true); 
@@ -47,8 +39,7 @@ if(isset($_POST)){
         $mail->setFrom('from@example.com', 'Mailer');
         $mail->addAddress('samnsimson@gmail.com', 'Sam N Simson');     // Add a recipient
         $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('samcladson08@gmail.com');
-        $mail->addBCC('bcc@example.com');
+        //$mail->addCC('samcladson08@gmail.com');
     
         //Content
         $mail->isHTML(true);                                  // Set email format to HTML
@@ -56,8 +47,9 @@ if(isset($_POST)){
         $mail->Body    = $message;
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     
-        $mail->send();
-        echo 'Message has been sent';
+        if($mail->send()){
+            return '200';
+        };
     } catch (Exception $e) {
         echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
